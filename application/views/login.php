f3dcfbbb8be95da75dab07df68c7617e8fbdad3a
Donny <mr.donny2008@gmail.com>
2017-12-10 19:36:20 +0700

    Initial commit

---
+++ application/views/login.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Database PMKS  Bina Netra Wyata Guna Bandung </title>
+</head>
+<body>
+
+<div id="container">
+	<h1>Login Page</h1>
+
+	
+</div>
+
+</body>
+</html>
